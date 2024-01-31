b6613a6fe64e86b0881bda98f744bc9693c8e0a3
ifatinha <ifatinha99@gmail.com>
2024-01-31 20:33:36 -0300

    logica concluida

--- functions/function.js
+++ functions/function.js
@@ -7,34 +7,64 @@ function randomMovie() {
 }
 
 function hideFilm(film) {
-    const hiddenMovie = "-".repeat(film.length);
-    return hiddenMovie;
+    let hiddenMovie = "-".repeat(film.length);
+    return hiddenMovie.split("");
 }
 
 function game(film, hiddenFilme) {
-    let chances = 5;
+    let chances = 4;
+    let won = false;
+
     console.log(`Adivinhe o nome do com ${hiddenFilme.length} letras.`);
-
     let arrayFilm = film.split("");
+    console.log(arrayFilm);
 
-    for (let i = 0; i < arrayFilm.length; i++) {
+    do {
+
+        console.log("Filme: " + hiddenFilme.join(""));
         let letra = scanner.question("Digite uma letra: ");
 
-        if (chances > 0) {
-            console.log(`Boa sorte! Você tem ${chances}.`);
+        const achou = arrayFilm.find((letter) => {
+            return letter.toLowerCase() === letra.toLowerCase();
+        })
 
+        arrayFilm.forEach((letter, index) => {
+            if (letter === " ") {
+                hiddenFilme[index] = " ";
+            }
+        })
+
+        if (achou !== undefined) {
+            arrayFilm.forEach((letter, index) => {
+                if (letter.toLowerCase() === letra.toLowerCase()) {
+                    hiddenFilme[index] = letra;
+                }
+            })
         } else {
-            console.log("Suas chances acabaram! Você Perder!!!!");
+            chances--;
+            if (chances === 0) {
+                console.log("Suas chances acabaram! VOCÊ PERDEU!!!!");
+                won = true;
+            } else {
+                console.log(`OPÇÃO ERRADA! Você ainda tem ${chances} chance(s).`);
+            }
         }
 
-        chances--;
-    }
+        if (hiddenFilme.join("").toLowerCase() === arrayFilm.join("").toLowerCase()) {
+            console.clear();
+            console.log("Parabéns!!! Você acertou");
+            console.log(`SUA RESPOSTA: ${hiddenFilme.join("").toCapitalize()}`);
+            console.log(`RESPOSTA: ${arrayFilm.join("")}`);
+            won = true;
+        }
+
+    } while (chances > 0 && won === false);
 }
 
 function init() {
     const film = randomMovie();
-    const hiddenWord = hideFilm(film);
-    game(film, hiddenWord);
+    const hiddenWords = hideFilm(film);
+    game(film, hiddenWords);
 }
 
 export default { init }
